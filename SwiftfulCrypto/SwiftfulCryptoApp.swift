32adffd69fe1e4f2b450fea30a1dd94bbebf9bc3
Sahas Pasya <pasyasahas17@gmail.com>
2024-10-28 10:15:10 -0500

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Pasya Sahas on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
